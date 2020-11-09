7b12ce5ef744b91b2553a1140e762c94e0a44984
Bryan Chontasi <33289907+BryanChontasi@users.noreply.github.com>
2020-11-09 22:25:04 +0000

    Update Exercice517_DisplayPyramid.java

--- Chapter 5/Exercice517_DisplayPyramid.java
+++ Chapter 5/Exercice517_DisplayPyramid.java
@@ -9,8 +9,14 @@ public class Exercice517_DisplayPyramid {
 		 * (Display pyramid) Write a program that prompts the user to enter an integer
 		 * from 1 to 15 and displays a pyramid, as shown in the following sample run;
 		 * 
-		 * Enter the numbers of lines: 7 7 6 5 4 3 2 1 2 3 4 5 6 7 6 5 4 3 2 1 2 3 4 5 6
-		 * 5 4 3 2 1 2 3 4 5 4 3 2 1 2 3 4 3 2 1 2 3 2 1 2 1
+		 * Enter the numbers of lines: 7 
+		 * 7 6 5 4 3 2 1 2 3 4 5 6 7
+		 * 6 5 4 3 2 1 2 3 4 5 6
+		 * 5 4 3 2 1 2 3 4 5 
+		 * 4 3 2 1 2 3 4 
+		 * 3 2 1 2 3 
+		 * 2 1 2 
+		 * 1
 		 * 
 		 * Bryan Chontasi
 		 */
