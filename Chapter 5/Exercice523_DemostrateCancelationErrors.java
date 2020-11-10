31bc8c73059d623a1ea94b54f5865c85412b4971
Bryan Chontasi <33289907+BryanChontasi@users.noreply.github.com>
2020-11-10 17:32:18 +0000

    Create Exercice523_DemostrateCancelationErrors.java

---
+++ Chapter 5/Exercice523_DemostrateCancelationErrors.java
@@ -0,0 +1,37 @@
+package Chapter5;
+
+public class Exercice523_DemostrateCancelationErrors {
+
+	public static void main(String[] args) {
+		/*
+		 * (Demonstrate cancellation errors) A cancellation error occurs when you are
+		 * manipulating a very large number with a very small number. The large number
+		 * may cancel out the smaller number. For example, the result of 100000000.0 +
+		 * 0.000000001 is equal to 100000000.0. To avoid cancellation errors and obtain
+		 * more accurate results, carefully select the order of computation. For
+		 * example, in computing the following series, you will obtain more accurate
+		 * results by computing from right to left rather than from left to right: 1 + 1
+		 * / 2 + 1 / 3 + ... + 1 / n Write a program that compares the results of the
+		 * summation of the preceding series, computing from left to right and from
+		 * right to left with n = 50000.
+		 * 
+		 * Bryan Chontasi
+		 */
+
+		int n = 50000;
+		double result1 = 0;
+		double result2 = 0;
+		
+		//sum left to right
+		for (int i = 1; i <= n; i++) {
+			result1 += 1.0 / i;
+		}
+		System.out.println("Computing from left to right: " + result1);
+		
+		//sum right to left
+		for (int i = n; i >= 1; i--) {
+			result2 += 1.0 / i;
+		}
+		System.out.println("Computing from right to left: " + result2);
+	}
+}
